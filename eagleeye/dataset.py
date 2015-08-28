348039e5e783f142b7542e55551e5d5cf0143968
gwillz <gwillz@internode.on.net>
2015-08-28 14:17:47 +0930

    updated mapping tool

--- eagleeye/dataset.py
+++ eagleeye/dataset.py
@@ -2,7 +2,7 @@
 # Project Eagle Eye
 # Group 15 - UniSA 2015
 # Gwilyn Saunders
-# version 0.1.2
+# version 0.2.3
 # 
 # Wraps a CSV reader and stores additional data
 # Automates routine functions
@@ -11,7 +11,7 @@
 import os, csv
 
 class Dataset:
-    def __init__(self, path, id, deci_places=2, delimiter=","):
+    def __init__(self, path, delimiter=","):
         # file ojects
         self.f = open(path, 'r')
         self.r = csv.reader(self.f, delimiter=delimiter)
@@ -19,34 +19,37 @@ class Dataset:
         # properties
         self._delim = delimiter
         self._path = path
-        self._id = "{{:0>{0}d}}".format(deci_places).format(id)
         self._name = os.path.basename(path).split("_")[-1].split(".")[0].replace("-", " ")
         
         # working vars
         self._row = None
         self._flash = False
-        self._line = 0
+        self._at = 0
     
     # getters
     def name(self):  return self._name
     def path(self):  return self._path
-    def id(self):    return self._id
-    def line(self):  return self._line
+    def at(self):    return self._at
     def flash(self): return self._flash
     
-    def verifyFlash(self):
-        if self._flash and self._row[1] != 'F':
-            self._flash = False
-    
     def row(self):
+        # load first row
         if self._row == None:
             self.next()
         return self._row
     
     def next(self):
-        try:
+        try: # load row
             self._row = self.r.next()
-            self._line += 1
+            self._at += 1
+            
+            # show warning for old-style datasets
+            if self._flash and self._row[1] == 'F':
+                print "WARNING: use of old type datasets are not supported"
+            
+            # test for flash
+            self._flash = (self._row[1] == 'F')
+            
             return True
         except StopIteration:
             return False
@@ -54,12 +57,12 @@ class Dataset:
     def reel(self):
         while True:
             if not self.next():
-                print "bad dataset:", self._name, ": EOF too soon at line:", self._line
-                raise StopIteration
+                print "bad dataset:", self._name, ": EOF too soon at line:", self._at
+                raise StopIterations
             
-            if self._row[1] == 'F':
-                self._flash = True
+            if self._flash:
                 break
+        self._flash = False
         
     def close(self):
         self.f.close()
