56b989da3038ca659fb0a89ed0b4d5e3df31bd62
gwillz <gwillz@internode.on.net>
2015-09-11 12:49:00 +0930

    removed unused eagleeye libraries

--- eagleeye/__init__.py
+++ eagleeye/__init__.py
@@ -11,12 +11,9 @@ from cv_keys import Key
 from cv_flags import CVFlag
 from buff_cap import BuffCap
 from buff_split_cap import BuffSplitCap
-from split_cap import SplitCapture
 from dataset import Dataset
-from buff_dataset import Buffset
 from xmlset import Xmlset
 from mem_dataset import Memset
-from viconsocket import ViconSocket
 from sleeper import Sleeper
 from marker_tool import marker_tool
 from mapping_func import Mapper
