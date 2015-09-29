4cfab3b6d5021aa94b02930a7e53f18dc7fbb18a
gwillz <gwillz@internode.on.net>
2015-09-29 23:21:42 +0930

    added presets to save options

--- custom_widgets/save_options_dialog.py
+++ custom_widgets/save_options_dialog.py
@@ -4,8 +4,6 @@ from PyQt4 import uic
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
-from eagleeye import marker_tool
-
 class SaveOptionsDialog(QDialog):
     def __init__(self, parent, edit_fields):
         QDialog.__init__(self, parent)
@@ -40,9 +38,9 @@ class SaveOptionsDialog(QDialog):
                 self.ui.vicondata.setChecked(True)
         
         for i in ["dataset_map", "dataset_ann", "evaluation", "comparison"]:
-                if i in edit_fields:
-                    self.ui.__dict__[i].setEnabled(True)
-                    self.ui.__dict__[i].setChecked(True)
+            if i in edit_fields:
+                self.ui.__dict__[i].setEnabled(True)
+                self.ui.__dict__[i].setChecked(True)
         
         # enable dataset group checkbox
         if self.ui.dataset_map.isEnabled() \
@@ -56,6 +54,15 @@ class SaveOptionsDialog(QDialog):
         self.dataset_mov.toggled.connect(self.check_children)
         self.dataset_check.toggled.connect(self.check_children)
         
+        # presets
+        self.ui.everything_button.clicked.connect(self.preset_everything)
+        self.ui.novideos_button.clicked.connect(self.preset_novideos)
+        self.ui.typical_button.clicked.connect(self.preset_typical)
+        self.ui.minimal_button.clicked.connect(self.preset_minimal)
+        
+        self.ui.dataset_mov.clicked.connect(lambda: self.ui.novideos_button.setChecked(False))
+        self.ui.trainer_mov.clicked.connect(lambda: self.ui.novideos_button.setChecked(False))
+        self.ui.comparison.clicked.connect(lambda: self.ui.novideos_button.setChecked(False))
     
     @pyqtSlot(bool)
     def check_children(self, val):
@@ -79,8 +86,41 @@ class SaveOptionsDialog(QDialog):
             if i in self.edit_fields \
                     and i in self.ui.__dict__:
                 self.ui.__dict__[i].setEnabled(val)
-        
     
+    @pyqtSlot()
+    def preset_everything(self):
+        # check all parent toggles
+        self.toggle_these(["calibration", "trainer_set", "dataset_mov", "dataset_map",
+                           "dataset_ann", "evaluation", "comparison",
+                           "chessboards", "trainer_csv", "trainer_mov", 
+                           "trainer_map", "vicondata"], True)
+        
+    @pyqtSlot(bool)
+    def preset_novideos(self, val):
+        self.toggle_these(["trainer_mov", "dataset_mov", "comparison"], not val)
+    
+    @pyqtSlot()
+    def preset_typical(self):
+        self.toggle_these(["calibration", "trainer_set", "dataset_mov", 
+                           "vicondata", "dataset_map", "dataset_ann", "evaluation"], True)
+        self.toggle_these(["chessboards", "trainer_csv", "trainer_mov", 
+                           "trainer_map", "comparison"], False)
+        
+    @pyqtSlot()
+    def preset_minimal(self):
+        self.toggle_these(["dataset_mov", "vicondata", "dataset_map", "dataset_ann"], True)
+        self.toggle_these(["chessboards", "calibration", "trainer_csv", "trainer_mov", 
+                           "trainer_map", "trainer_set", "comparison", "evaluation"], False)
+    
+    @pyqtSlot()
+    def toggle_these(self, checkboxes, val):
+        for i in checkboxes:
+            if i in self.edit_fields \
+                    and i in self.ui.__dict__ \
+                    and self.ui.__dict__[i].isEnabled():
+                self.ui.__dict__[i].setChecked(val)
+    
+    # return a dict of checked fields with the respective input values
     def checked_fields(self):
         return_fields = {}
         
@@ -91,3 +131,26 @@ class SaveOptionsDialog(QDialog):
                 return_fields[k] = self.edit_fields[k]
         
         return return_fields
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    
+    edit_fields = {
+            "calibration": "",
+            "chessboards": "",
+            "trainer_set": "",
+            "trainer_csv": "",
+            "trainer_mov": "",
+            "trainer_map": "",
+            "dataset_mov": "",
+            "vicondata": "",
+            "dataset_map": "",
+            "dataset_ann": "",
+            "evaluation": "",
+            "comparison": ""}
+    
+    w = QMainWindow()
+    dialog = SaveOptionsDialog(w, edit_fields)
+    exit(dialog.exec_())
+    
